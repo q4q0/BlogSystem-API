307b534f90d555e019e5ff797bb3bb124e3dd32f
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-20 23:21:00 +0300

    💻 update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 /*--------- Setting up morgan middleware ---------*/
-
 app.use(morgan('dev'));
+
+/*--------- Setting up cors middleware ---------*/
+app.use(cors());
 
 /*--------- Setting up MySQL database connection ---------*/
 const db = require('./Config/db');
