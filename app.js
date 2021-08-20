4306b6f552d126dd15add22daeb3dad79f8b32d4
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-20 03:24:35 +0300

    👨‍🏫 update

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+/*--------- Setting up morgan middleware ---------*/
+
+app.use(morgan());
+
 /*--------- Setting up MySQL database connection ---------*/
+
 const dbConnection = require('./Config/db');
 dbConnection();
 
-/*--------- Setting up express body parser ---------*/
+/*--------- Setting up express body parser middleware ---------*/
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/*--------- Routes ---------*/
+/*--------- Require routes ---------*/
+
+const usersRoutes = require('./Routes/users.routes');
+const postsRoutes = require('./Routes/posts.routes');
+const tagsRoutes = require('./Routes/tags.routes');
+const categoriesRoutes = require('./Routes/categories.routes');
+
+/*--------- Setting up routes ---------*/
+
+app.use('/api/v1/users', usersRoutes);
+app.use('/api/v1/posts', postsRoutes);
+app.use('/api/v1/tags', tagsRoutes);
+app.use('/api/v1/categories', categoriesRoutes);
 
 module.exports = app;
