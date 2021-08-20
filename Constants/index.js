8cbb1b05cd8fc939b04077dd4adaa680282d605d
~/4bdull4h <s201830040@kfupm.edu.sa>
2021-08-20 17:59:02 +0300

    👨‍🏫 update

--- Constants/index.js
+++ Constants/index.js
@@ -7,7 +7,17 @@ module.exports = {
       EMAIL: 'email address is required',
       PASSWORD: 'password is required',
     },
+    VALIDATION_ERRORS: {
+      FIRST_NAME_INVALID_LENGTH:
+        'first name should be between 2 and 32 characters long',
+      LAST_NAME_INVALID_LENGTH:
+        'last name should be between 2 and 32 characters long',
+      FIRST_NAME_NOT_ALPHA: 'first name must be letters only',
+      LAST_NAME_NOT_ALPHA: 'last name must be letters only',
+      USERNAME_CONFLICT: 'username is already exists',
+      EMAIL_CONFLICT: 'email is already exists',
+      USER_NAME_INVALID_LENGTH:
+        'username should be between 6 and 32 characters long',
+    },
   },
 };
-
-
